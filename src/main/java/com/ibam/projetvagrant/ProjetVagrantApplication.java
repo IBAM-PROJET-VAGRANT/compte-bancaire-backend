98a4493de2f056591fee9c2595f7be89f83258f8
Osie <sielamoussaouattara@gmail.com>
2024-07-09 22:38:09 +0000

    initial commit

---
+++ src/main/java/com/ibam/projetvagrant/ProjetVagrantApplication.java
@@ -0,0 +1,13 @@
+package com.ibam.projetvagrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetVagrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetVagrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibam/projetvagrant/ProjetVagrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibam.projetvagrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetVagrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
